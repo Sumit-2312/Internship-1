f046bca3c6d9626fc7d7b42527b3d1e0542b5ca6
Sumit <sumitsongh160@gmail.com>
2025-04-11 00:37:42 +0530

    made responsive

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App2(){
     <div className="w-full max-w-[100vw]
     min-h-screen flex flex-col items-center justify-start p-4 bg-gradient-to-t from-purple-700 to-black gap-10 overflow-x-hidden">
              <Navbar/>
-             <div className="w-[900px]   flex flex-col items-center justify-start gap-10">
+             <div className="max-w-[900px] flex flex-col items-center justify-start gap-10">
                <ProfileCard/>
                <Content/>
              </div>
--- Frontend/src/components/Posts.tsx
+++ Frontend/src/components/Posts.tsx
@@ -10,13 +10,13 @@ export default function Posts() {
  
 
   return (
-    <div className="w-full h-fit bg-gray-500 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 p-4 rounded-2xl">
+    <div className="w-full h-fit bg-gray-500 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-3 sm:p-4 py-6 px-0  rounded-2xl ">
      
       {Posts.map((Posts:any) => (
        <Link to={`/post/${Posts.id}`} >
           <div 
             key={Posts.id}
-            className="relative group h-96 bg-gray-300 hover:cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out rounded-2xl shadow-lg shadow-gray-800 overflow-hidden"
+            className="relative group h-[450px] md:h-96 bg-gray-300 hover:cursor-pointer hover:scale-105 transition-transform duration-300 ease-in-out sm:w-full w-4/6 m-auto rounded-2xl shadow-lg shadow-gray-800 overflow-hidden"
           >
               <img className="h-full w-full " src={Posts.imageUrl} alt="Image" />
 
