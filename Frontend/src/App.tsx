bb35bd903ce47224f29928258f6b56cf19de53ac
Sumit <sumitsongh160@gmail.com>
2025-04-13 09:01:23 +0530

    added login button in dashboard

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,12 +5,24 @@ import {Routes,Route} from 'react-router-dom';
 import InstagramPost from "./components/IndividualPost";
 import InstagramLogin from "./LoginPage";
 import Error from "./components/error-component";
+import { useEffect, useState } from "react";
 
 function App2(){
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [accessToken, setAccessToken] = useState("");
+  const [userId, setUserId] = useState("");
+
+  useEffect(()=>{
+    // navbar component will check if the user is logged in or not
+    // if not then it will redirect to the login page
+    
+  })
+
   return(
     <div className="w-full max-w-[100vw]
     min-h-screen flex flex-col items-center justify-start p-4 bg-gradient-to-t from-purple-700 to-black gap-10 overflow-x-hidden">
-             <Navbar/>
+             <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setAccessToken={setAccessToken} setUserId={setUserId} />
              <div className="max-w-[900px] flex flex-col items-center justify-start gap-10">
                <ProfileCard/>
                <Content/>
--- Frontend/src/components/navbar.tsx
+++ Frontend/src/components/navbar.tsx
@@ -1,8 +1,46 @@
 import { FaInstagram } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
+import { useEffect } from "react";
+import Button from "./Button";
+import {useLocation, useNavigate } from "react-router-dom";
 
 
-export default function Navbar(){
+export default function Navbar({isLoggedIn,setIsLoggedIn, setAccessToken}:any) {
+
+    const navigate = useNavigate();
+    const Location = useLocation();
+
+    useEffect(()=>{
+
+        const params = new URLSearchParams(Location.search);
+        const access_token = params.get("access_token");
+        console.log(params);
+        console.log(access_token);
+
+        if(!access_token){
+            // this means the user is not redirecting from the login page and we need to check the local storage for the logged in status
+            const LoggedIn = localStorage.getItem("LoggedIn");
+            if(LoggedIn === "true"){
+                setIsLoggedIn((true));
+                // this means the user is already logged in and we need to fetch the instagram profile details
+                
+            }
+            else{
+                navigate('/login')
+                // user will login from there and at last he will be redirected to home page
+            }
+        }
+ 
+        else{
+            // this means the user is redirecting from the login page and we need to check the access token
+            setIsLoggedIn(true);
+            setAccessToken(access_token);
+            localStorage.setItem("LoggedIn", "true");
+        }
+
+      
+    },[Location.search])
+
     return (
         <div className="w-full h-10  p-4  flex items-center justify-between ">
 
@@ -13,8 +51,11 @@ export default function Navbar(){
               <h1 className="bg-gradient-to-r from-purple-600  to-purple-100 text-2xl font-bold bg-clip-text text-transparent  ">Instagram</h1>
             </div>
 
-            <div className="h-10 w-10 rounded-full bg-gray-200 ">
-                <CgProfile className="h-full w-full text-gray-100 "/>
+            <div className="flex items-center justify-center gap-4">
+                <Button text={`${isLoggedIn ? "Log Out" : "Log In"}`} />
+                <div className="h-10 w-10 rounded-full bg-gray-200 ">
+                    <CgProfile className="h-full w-full text-gray-100 "/>
+                </div>
             </div>
 
         </div>
